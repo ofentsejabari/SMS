55991fb3cfed065a88f02a073e1826eaab097825
cranemoile <cranemoile@gmail.com>
2018-04-09 16:56:29 +0200

    ADDING SOME COMMENNTS

--- src/mysqldriver/InventoryQuery.java
+++ src/mysqldriver/InventoryQuery.java
@@ -5,6 +5,9 @@
  */
 package mysqldriver;
 
+import inventorymanagement.Facilities;
+import inventorymanagement.FacilitiesStatus;
+import inventorymanagement.FacilitiesType;
 import inventorymanagement.Inventory;
 import java.sql.ResultSet;
 import javafx.collections.FXCollections;
@@ -58,5 +61,107 @@ public class InventoryQuery {
              return item;
         }
     }
-
+      
+        public static ObservableList<Facilities>  facilitiesList (String filter){
+        ObservableList<Facilities> item = FXCollections.observableArrayList();
+        try{
+            String query = " SELECT facilitiesID,facilitiesName,facilitiesTypeID,"
+                    + "deptID,facilitiesStatus,facilitiesCapacity,schoolID"
+                         + " FROM `facilities`,`school`"
+                         + " WHERE `facilities`.`schoolID` = `school`.`id`";
+            
+            if("ALL".equalsIgnoreCase(filter)){
+               query = " SELECT facilitiesID,facilitiesName,facilitiesTypeID,"
+                    + "deptID,facilitiesStatus, facilitiesCapacity,schoolID"
+                         + " FROM `facilities`,`school`"
+                         + " WHERE `facilities`.`schoolID` = `school`.`id`";
+            }
+            
+            
+            ResultSet result = STATEMENT.executeQuery(query);
+            
+            while(result.next()){
+                
+                /* Inventory(String inventoryID,String inventoryName, String inventoryCost,String inventoryLocation,
+            String inventoryBatch,String inventoryDate,String inventoryStaff,String inventoryQuantity)
+                */
+                item.add(new Facilities(result.getString("facilitiesID"),result.getString("facilitiesName")
+                        ,result.getString("facilitiesTypeID"),result.getString("deptID"),result.getString("facilitiesStatus"),
+                        result.getString("facilitiesCapacity"),result.getString("schoolID")));
+            }
+            
+            return item;
+        } 
+        catch(Exception ex){
+             System.out.println(ex.getMessage());
+             return item;
+        }
+    }
+        
+      //  
+        public static ObservableList<FacilitiesType>  facilitiesTypeList (String filter){
+        ObservableList<FacilitiesType> item = FXCollections.observableArrayList();
+        try{
+            String query = " SELECT `facilities_type`.`facilitiesTypeID`,`facilitiesTypeName`,"
+                    + "`deptID`,`facilitiesStatus`,`facilitiesCapacity`,`schoolID`"
+                         + " FROM `facilities_type`,`facilities`,`school`"
+                         + " WHERE `facilities`.`schoolID` = `school`.`id`";
+            
+            if("ALL".equalsIgnoreCase(filter)){
+                    query = " SELECT `facilities_type`.facilitiesTypeID,facilitiesTypeName,"
+                          + "`deptID`,`facilitiesStatus`, `facilitiesCapacity`,`schoolID`"
+                          + " FROM `facilities_type`,`facilities`,`school`"
+                            + " WHERE `facilities`.`schoolID` = `school`.`id`";
+            }
+            
+            
+            ResultSet result = STATEMENT.executeQuery(query);
+            
+            while(result.next()){
+                
+                item.add(new FacilitiesType(result.getString("facilitiesTypeID"),
+                        result.getString("facilitiesTypeName"),""));
+            }
+            
+            return item;
+        } 
+        catch(Exception ex){
+             System.out.println(ex.getMessage());
+             return item;
+        }
+    }
+        
+        
+    public static ObservableList<FacilitiesStatus>  facilitiesStatusList (String filter){
+        ObservableList<FacilitiesStatus> item = FXCollections.observableArrayList();
+        try{
+            String query = " SELECT facilitiesStatusID,facilitiesStatusResource,facilitiesStatusAvailable"
+                         + ",facilitiesStatusDamaged"
+                         + " FROM `facilities_status`"
+                         + " WHERE 1";
+            
+            if("ALL".equalsIgnoreCase(filter)){
+                    query = " SELECT facilitiesStatusID,facilitiesStatusResource,facilitiesStatusAvailable"
+                         + ",facilitiesStatusDamaged"
+                         + " FROM `facilities_status`"
+                         + " WHERE 1";
+            }
+            
+            
+            ResultSet result = STATEMENT.executeQuery(query);
+            
+            while(result.next()){
+                
+                item.add(new FacilitiesStatus(result.getString("facilitiesStatusID"),
+                        result.getString("facilitiesStatusResource"),result.getString("facilitiesStatusAvailable"),result.getString("facilitiesStatusDamaged")));
+            }
+            
+            return item;
+        } 
+        catch(Exception ex){
+             System.out.println(ex.getMessage());
+             return item;
+        }
+    }
+                
 }
